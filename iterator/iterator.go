372ff16e12ea6db29a83370e55f9308a18f7127a
victorbetoni <victorbetoni@protonmail.com>
2023-11-14 13:50:16 -0300

    iterator

---
+++ iterator/iterator.go
@@ -0,0 +1,47 @@
+package iterator
+
+import "github.com/victorbetoni/go-streams/streams"
+
+type Iterator[E any] struct {
+	collection []E
+	Current    *E
+	Index      int
+}
+
+func SliceIterator[E any](slice *[]E) *Iterator[E] {
+	return StreamIterator[E](streams.StreamOf[E](*slice...))
+}
+
+func StreamIterator[E any](stream *streams.Stream[E]) *Iterator[E] {
+	return &Iterator[E]{
+		collection: stream.Current,
+		Current:    &stream.Current[0],
+		Index:      0,
+	}
+}
+
+func (i *Iterator[E]) HasNext() bool {
+	return i.Index+1 < len(i.collection)
+}
+
+func (i *Iterator[E]) HasPrevious() bool {
+	return i.Index-1 >= 0
+}
+
+func (i *Iterator[E]) Next() bool {
+	if !i.HasNext() {
+		return false
+	}
+	i.Index++
+	i.Current = &i.collection[i.Index]
+	return true
+}
+
+func (i *Iterator[E]) Previous() bool {
+	if !i.HasPrevious() {
+		return false
+	}
+	i.Index--
+	i.Current = &i.collection[i.Index]
+	return true
+}
